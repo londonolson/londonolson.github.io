971bfe5c298e102a9b0df95ff25971c0bcc6b6b2
londonolson <londonolsonxc@gmail.com>
2019-03-30 19:15:31 -0500

    Added the ability to pull from mysql table

--- process.php
+++ process.php
@@ -41,4 +41,44 @@ mysqli_error($link);
 
 mysqli_close($link);
 
+//Pull data from the mysql table
+$link = mysqli_connect("localhost", "root", "", "demo");
+ 
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+$sql = "SELECT * FROM customers";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>name</th>";
+                echo "<th>adults</th>";
+                echo "<th>children</th>";
+                echo "<th>checkInDate</th>";
+                echo "<th>checkOutDate</th>";
+                echo "<th>phone</th>";
+                echo "<th>destination</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['adults'] . "</td>";
+                echo "<td>" . $row['checkInDate'] . "</td>";
+                echo "<td>" . $row['checkOutDate'] . "</td>";
+                echo "<td>" . $row['phone'] . "</td>";
+                echo "<td>" . $row['destination'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not execute $sql. " . mysqli_error($link);
+}
+ 
+mysqli_close($link);
 ?>
